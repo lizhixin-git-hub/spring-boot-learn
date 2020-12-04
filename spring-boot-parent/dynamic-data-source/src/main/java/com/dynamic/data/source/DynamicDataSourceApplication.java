e004bebdd26128ae4c51c8f5e823cc8f176e96d7
lizhixin-git-hub <15907300798@163.com>
2020-12-04 09:35:51 +0800

    添加资料链接

--- spring-boot-parent/dynamic-data-source/src/main/java/com/dynamic/data/source/DynamicDataSourceApplication.java
+++ spring-boot-parent/dynamic-data-source/src/main/java/com/dynamic/data/source/DynamicDataSourceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //需排除springboot自动配置类加载，数据源配置类 @Primary 会出现重复依赖问题
+//多数据源事务https://www.cnblogs.com/cq-yangzhou/p/10945779.html
+//https://github.com/lishuangqi/springboot-mybatis-druid-atomikos/
+//https://www.cnblogs.com/lixianguo/p/12522442.html
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.dynamic.data.source.mapper")
 public class DynamicDataSourceApplication {
