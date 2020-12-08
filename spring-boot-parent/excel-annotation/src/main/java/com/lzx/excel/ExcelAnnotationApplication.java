7d14ec818224c3891c167b1936b1618b3ddc133d
lizhixin-git-hub <15907300798@163.com>
2020-12-08 19:47:40 +0800

    修改mybatis-plus版本

--- spring-boot-parent/excel-annotation/src/main/java/com/lzx/excel/ExcelAnnotationApplication.java
+++ spring-boot-parent/excel-annotation/src/main/java/com/lzx/excel/ExcelAnnotationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 项目中使用自定义注解导出/导入excel,也可以使用easy-poi插件
  */
+//https://github.com/allanzhuo/myport.git
+//https://mp.weixin.qq.com/s/2_M8WOUXSa7j2N4IXUyWZw
 @SpringBootApplication
 public class ExcelAnnotationApplication {
 
