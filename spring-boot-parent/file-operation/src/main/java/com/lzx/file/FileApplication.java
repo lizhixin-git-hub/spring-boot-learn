3d6af43c72708083bf8d2f8d334ba6a8bd0bfb10
lizhixin-git-hub <15907300798@163.com>
2021-02-02 19:24:17 +0800

    添加资料路径

--- spring-boot-parent/file-operation/src/main/java/com/lzx/file/FileApplication.java
+++ spring-boot-parent/file-operation/src/main/java/com/lzx/file/FileApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * https://github.com/gaoyuyue/MyUploader-Backend
  * https://github.com/gaoyuyue/MyUploader
+ * https://gitee.com/mzxc_admin/gomyck-fastdfs-spring-boot-starter
  */
 @SpringBootApplication
 public class FileApplication {
